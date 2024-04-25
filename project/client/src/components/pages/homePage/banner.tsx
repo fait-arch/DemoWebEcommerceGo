a0b7e77f520c5f28255023fc7e2428da2def9588
fait-arch <jdmoromenacho@email.com>
2024-04-25 11:32:14 -0500

    Copia de vercion estable

--- project/client/src/components/pages/homePage/banner.tsx
+++ project/client/src/components/pages/homePage/banner.tsx
@@ -5,12 +5,10 @@ const Banner: React.FC = () => {
 		<section className="bg-white dark:bg-gray-900 rounded-bl-lg rounded-br-lg">
 			<div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
 				<h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-					daniel
+					E-commer con Golang
 				</h1>
 				<p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-					Here at Flowbite we focus on markets where technology,
-					innovation, and capital can unlock long-term value and drive
-					economic growth.
+					Si puedes imaginarlo puedes programarlo.
 				</p>
 				<div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
 					<a
